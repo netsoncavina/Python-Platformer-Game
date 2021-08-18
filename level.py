b68ee4affd304e1d520f911fa4cdf493a1a7ce02
Netson <netsoncavina@gmail.com>
2021-08-18 11:23:18 -0300

    Adição das colisões com o cenario

--- level.py
+++ level.py
@@ -41,12 +41,44 @@ class Level:
             self.world_shift = 0
             player.speed = 5
 
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+        
+        player.rect.x += player.direction.x * player.speed
+        # Testes de colisão horizontal
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+    
+    def vertical_movement_collision(self):
+        player = self.player.sprite
+        player.apply_gravity()
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0 # Reseta a gravidade
+                elif player.direction.y < 0:
+                    player.rect.top = sprite.rect.bottom
+                    player.direction.y = 0 # Evita que "grude" no teto
+
+
+    
+
+
     def run(self):
         # Level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        self.scroll_x()
 
         # Player
         self.player.update()
+        self.horizontal_movement_collision()
+        self.vertical_movement_collision()
         self.player.draw(self.display_surface)
-        self.scroll_x()
+        
