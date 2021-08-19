8c73539eb76d4127f36442b23b0fee8971f81dde
Netson <netsoncavina@gmail.com>
2021-08-19 00:01:46 -0300

    Adição dos efeitos de poeira quando anda, pula e cai

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from tiles import Tile
 from settings import tile_size, screen_width
 from player import Player
+from particles import ParticleEffect
 
 class Level:
     def __init__(self,level_data,surface):
@@ -10,7 +11,34 @@ class Level:
         self.setup_level(level_data)
         self.world_shift = 0
         self.current_x = 0
+
+        # Dust
+        self.dust_sprite = pygame.sprite.GroupSingle()
+        self.player_on_ground = False
     
+    def create_jump_particles(self,pos):
+        if self.player.sprite.facing_right:
+            pos -= pygame.math.Vector2(10,5)
+        else:
+            pos += pygame.math.Vector2(10,-5)
+        jump_particle_sprite = ParticleEffect(pos, 'jump')
+        self.dust_sprite.add(jump_particle_sprite)
+
+    def get_player_on_ground(self):
+        if self.player.sprite.on_ground:
+            self.player_on_ground = True
+        else:
+            self.player_on_ground = False
+    
+    def create_landing_dust(self):
+        if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
+            if self.player.sprite.facing_right:
+                offset = pygame.math.Vector2(10,15)
+            else:
+                offset = pygame.math.Vector2(-10,15)
+            fall_dust_particle = ParticleEffect(self.player.sprite.rect.midbottom - offset, 'land')
+            self.dust_sprite.add(fall_dust_particle)
+
     def setup_level(self,layout):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
@@ -24,7 +52,7 @@ class Level:
                     tile = Tile((x,y),tile_size)
                     self.tiles.add(tile)
                 if cell == 'P':
-                    player_sprite = Player((x,y))
+                    player_sprite = Player((x,y), self.display_surface,self.create_jump_particles)
                     self.player.add(player_sprite)
 
     def scroll_x(self):
@@ -62,7 +90,6 @@ class Level:
             player.on_left = False
         if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
             player.on_right = False
-
     
     def vertical_movement_collision(self):
         player = self.player.sprite
@@ -84,11 +111,11 @@ class Level:
         if player.on_ceiling and player.direction.y > 0:
             player.on_ceiling = False
 
-
-    
-
-
     def run(self):
+       # Dust Particles
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
+       
         # Level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
@@ -97,6 +124,8 @@ class Level:
         # Player
         self.player.update()
         self.horizontal_movement_collision()
+        self.get_player_on_ground()
         self.vertical_movement_collision()
+        self.create_landing_dust()
         self.player.draw(self.display_surface)
         
