9f1636ede4b20400e9250a395c93e3d1acdf7ebc
Netson <netsoncavina@gmail.com>
2021-08-26 16:24:10 -0300

    Integração com o player e melhorias no display da agua (agora cobre o mapa todo)

--- level.py
+++ level.py
@@ -11,9 +11,15 @@ class Level:
     def __init__(self,level_data,surface):
         # Level setup
         self.display_surface = surface
-        self.setup_level(level_data)
-        self.world_shift = -4
-        self.current_x = 0
+        # self.setup_level(level_data)
+        self.world_shift = 0
+        self.current_x = None
+
+        # Player setup
+        player_layout = import_csv_layout(level_data['player'])
+        self.player = pygame.sprite.GroupSingle()
+        self.goal = pygame.sprite.GroupSingle()
+        self.player_setup(player_layout)  
 
         # Terrain setup
         terrain_layout = import_csv_layout(level_data['terrain'])
@@ -50,9 +56,8 @@ class Level:
         # Decoration
         self.sky = Sky(8)
         level_width = len(terrain_layout[0])*tile_size
-        self.water = Water(screen_height - 40, level_width+500)
+        self.water = Water(screen_height - 40, level_width)
         self.clouds = Cloud(400, level_width, 15)
-
 
         # Dust
         self.dust_sprite = pygame.sprite.GroupSingle()
@@ -95,106 +100,119 @@ class Level:
                         sprite = Tile(tile_size,x,y)
                     sprite_group.add(sprite)
 
-
-
         return sprite_group
 
-    # def create_jump_particles(self,pos):
-    #     if self.player.sprite.facing_right:
-    #         pos -= pygame.math.Vector2(10,5)
-    #     else:
-    #         pos += pygame.math.Vector2(10,-5)
-    #     jump_particle_sprite = ParticleEffect(pos, 'jump')
-    #     self.dust_sprite.add(jump_particle_sprite)
-
-    # def get_player_on_ground(self):
-    #     if self.player.sprite.on_ground:
-    #         self.player_on_ground = True
-    #     else:
-    #         self.player_on_ground = False
-    
-    # def create_landing_dust(self):
-    #     if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
-    #         if self.player.sprite.facing_right:
-    #             offset = pygame.math.Vector2(10,15)
-    #         else:
-    #             offset = pygame.math.Vector2(-10,15)
-    #         fall_dust_particle = ParticleEffect(self.player.sprite.rect.midbottom - offset, 'land')
-    #         self.dust_sprite.add(fall_dust_particle)
-
-
-    # Talvez apagavel
-    def setup_level(self,layout):
-        self.tiles = pygame.sprite.Group()
-        self.player = pygame.sprite.GroupSingle()
-        # Encontrando onde o "X" está
-        for row_index,row in enumerate(layout):
-            for col_index,cell in enumerate(row):
+    def player_setup(self,layout):
+        for row_index, row in enumerate(layout):
+            for col_index,val in enumerate(row):
                 x = col_index * tile_size
                 y = row_index * tile_size
-                
-                if cell=='X':
-                    tile = Tile((x,y),tile_size)
-                    self.tiles.add(tile)
-                if cell == 'P':
-                    player_sprite = Player((x,y), self.display_surface,self.create_jump_particles)
-                    self.player.add(player_sprite)
+                if val =='0':
+                    sprite = Player((x,y),self.display_surface,self.create_jump_particles)
+                    self.player.add(sprite)
+                if val == '1':
+                    hat_surface = pygame.image.load('graphics\character\hat.png').convert_alpha()
+                    sprite = StaticTile(tile_size,x,y,hat_surface)
+                    self.goal.add(sprite)
 
-    # def scroll_x(self):
-    #     player = self.player.sprite
-    #     player_x = player.rect.centerx
-    #     direction_x = player.direction.x
+    def create_jump_particles(self,pos):
+        if self.player.sprite.facing_right:
+            pos -= pygame.math.Vector2(10,5)
+        else:
+            pos += pygame.math.Vector2(10,-5)
+        jump_particle_sprite = ParticleEffect(pos, 'jump')
+        self.dust_sprite.add(jump_particle_sprite)
 
-    #     if player_x < (screen_width / 4) and direction_x < 0: # Precisa ter passado de 1/4 da tela e estar se movendo para a esquerda
-    #         self.world_shift = 5
-    #         player.speed = 0
-    #     elif player_x > screen_width - (screen_width / 4) and direction_x > 0: # Precisa ter passado de 3/4 da tela e estar se movendo para a direita
-    #         self.world_shift = -5
-    #         player.speed = 0
-    #     else:
-    #         self.world_shift = 0
-    #         player.speed = 5
-
-    # def horizontal_movement_collision(self):
-    #     player = self.player.sprite
-        
-    #     player.rect.x += player.direction.x * player.speed
-    #     # Testes de colisão horizontal
-    #     for sprite in self.tiles.sprites():
-    #         if sprite.rect.colliderect(player.rect):
-    #             if player.direction.x < 0:
-    #                 player.rect.left = sprite.rect.right
-    #                 player.on_left = True
-    #                 self.current_x = player.rect.left
-    #             elif player.direction.x > 0:
-    #                 player.rect.right = sprite.rect.left
-    #                 player.on_right = True
-    #                 self.current_x = player.rect.right
-
-    #     if player.on_left and (player.rect.left < self.current_x or player.direction.x >= 0):
-    #         player.on_left = False
-    #     if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
-    #         player.on_right = False
+    def get_player_on_ground(self):
+        if self.player.sprite.on_ground:
+            self.player_on_ground = True
+        else:
+            self.player_on_ground = False
     
-    # def vertical_movement_collision(self):
-    #     player = self.player.sprite
-    #     player.apply_gravity()
+    def create_landing_dust(self):
+        if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
+            if self.player.sprite.facing_right:
+                offset = pygame.math.Vector2(10,15)
+            else:
+                offset = pygame.math.Vector2(-10,15)
+            fall_dust_particle = ParticleEffect(self.player.sprite.rect.midbottom - offset, 'land')
+            self.dust_sprite.add(fall_dust_particle)
 
-    #     for sprite in self.tiles.sprites():
-    #         if sprite.rect.colliderect(player.rect):
-    #             if player.direction.y > 0:
-    #                 player.rect.bottom = sprite.rect.top
-    #                 player.direction.y = 0 # Reseta a gravidade
-    #                 player.on_ground = True
-    #             elif player.direction.y < 0:
-    #                 player.rect.top = sprite.rect.bottom
-    #                 player.direction.y = 0 # Evita que "grude" no teto
-    #                 player.on_ceiling = True
+
+    # # Talvez apagavel
+    # def setup_level(self,layout):
+    #     self.tiles = pygame.sprite.Group()
+    #     self.player = pygame.sprite.GroupSingle()
+    #     # Encontrando onde o "X" está
+    #     for row_index,row in enumerate(layout):
+    #         for col_index,cell in enumerate(row):
+    #             x = col_index * tile_size
+    #             y = row_index * tile_size
+                
+    #             if cell=='X':
+    #                 tile = Tile((x,y),tile_size)
+    #                 self.tiles.add(tile)
+    #             if cell == 'P':
+    #                 player_sprite = Player((x,y), self.display_surface,self.create_jump_particles)
+    #                 self.player.add(player_sprite)
+
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < (screen_width / 4) and direction_x < 0: # Precisa ter passado de 1/4 da tela e estar se movendo para a esquerda
+            self.world_shift = 5
+            player.speed = 0
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0: # Precisa ter passado de 3/4 da tela e estar se movendo para a direita
+            self.world_shift = -5
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 5
+
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+        collidable_sprites = self.terrain_sprites.sprites() + self.crate_sprites.sprites() + self.fg_palm_sprites.sprites()
+
+        # Testes de colisão horizontal
+        for sprite in collidable_sprites:
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                    player.on_left = True
+                    self.current_x = player.rect.left
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+                    player.on_right = True
+                    self.current_x = player.rect.right
+
+        if player.on_left and (player.rect.left < self.current_x or player.direction.x >= 0):
+            player.on_left = False
+        if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
+            player.on_right = False
+    
+    def vertical_movement_collision(self):
+        player = self.player.sprite
+        player.apply_gravity()
+        collidable_sprites = self.terrain_sprites.sprites() + self.crate_sprites.sprites() + self.fg_palm_sprites.sprites()
+
+        for sprite in collidable_sprites:
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0 # Reseta a gravidade
+                    player.on_ground = True
+                elif player.direction.y < 0:
+                    player.rect.top = sprite.rect.bottom
+                    player.direction.y = 0 # Evita que "grude" no teto
+                    player.on_ceiling = True
         
-    #     if player.on_ground and player.direction.y < 0 or player.direction.y > 1:
-    #         player.on_ground = False
-    #     if player.on_ceiling and player.direction.y > 0:
-    #         player.on_ceiling = False
+        if player.on_ground and player.direction.y < 0 or player.direction.y > 1:
+            player.on_ground = False
+        if player.on_ceiling and player.direction.y > 0:
+            player.on_ceiling = False
 
     def enemy_collision_reverse(self):
         for enemy in self.enemy_sprites.sprites():
@@ -202,17 +220,10 @@ class Level:
                 enemy.reverse_speed()
 
     def run(self):
-       # Dust Particles
-        # self.dust_sprite.update(self.world_shift)
-        # self.dust_sprite.draw(self.display_surface)
-       
-        
-
         # Sky and clouds
         self.sky.draw(self.display_surface)
         self.clouds.draw(self.display_surface,self.world_shift)
 
-        
         #   Level tiles
         # Bg Palms
         self.bg_palm_sprites.update(self.world_shift)
@@ -247,19 +258,22 @@ class Level:
         # Water
         self.water.draw(self.display_surface,self.world_shift)
 
+        # Player Sprites
+        self.goal.update(self.world_shift)
+        self.goal.draw(self.display_surface)
         
-
-
-      
+        # Dust Particles
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
 
         # self.tiles.draw(self.display_surface)
-        # self.scroll_x()
+        self.scroll_x()
 
-        # # Player
-        # self.player.update()
-        # self.horizontal_movement_collision()
-        # self.get_player_on_ground()
-        # self.vertical_movement_collision()
-        # self.create_landing_dust()
-        # self.player.draw(self.display_surface)
+        # Player
+        self.player.update()
+        self.horizontal_movement_collision()
+        self.get_player_on_ground()
+        self.vertical_movement_collision()
+        self.create_landing_dust()
+        self.player.draw(self.display_surface)
         
