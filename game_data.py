eb42a7dfb840f5c158c9899d7087e2b245e23b63
Netson <netsoncavina@gmail.com>
2022-04-16 03:32:28 -0300

    changed level data

--- game_data.py
+++ game_data.py
@@ -14,21 +14,6 @@ level_0 = {
 }
 
 level_1 = {
-    'terrain': 'levels/1/level_1_terrain.csv',
-    'coins': 'levels/1/level_1_coins.csv',
-    'fg palms': 'levels/1/level_1_fg_Palms.csv',
-    'bg palms': 'levels/1/level_1_bg_Palms.csv',
-    'crates': 'levels/1/level_1_crates.csv',
-    'enemies': 'levels/1/level_1_enemies.csv',
-    'constraints': 'levels/1/level_1_constraints.csv',
-    'player': 'levels/1/level_1_player.csv',
-    'grass': 'levels/1/level_1_grass.csv',
-    'node_pos': (300,220),
-	'unlock': 2,
-	'node_graphics': './graphics/overworld/2',
-}
-
-level_2 = {
     'terrain': 'levels/2/level_2_terrain.csv',
     'coins': 'levels/2/level_2_coins.csv',
     'fg palms': 'levels/2/level_2_fg_Palms.csv',
@@ -38,6 +23,21 @@ level_2 = {
     'constraints': 'levels/2/level_2_constraints.csv',
     'player': 'levels/2/level_2_player.csv',
     'grass': 'levels/2/level_2_grass.csv',
+    'node_pos': (300,220),
+	'unlock': 2,
+	'node_graphics': './graphics/overworld/2',
+}
+
+level_2 = {
+    'terrain': 'levels/1/level_1_terrain.csv',
+    'coins': 'levels/1/level_1_coins.csv',
+    'fg palms': 'levels/1/level_1_fg_Palms.csv',
+    'bg palms': 'levels/1/level_1_bg_Palms.csv',
+    'crates': 'levels/1/level_1_crates.csv',
+    'enemies': 'levels/1/level_1_enemies.csv',
+    'constraints': 'levels/1/level_1_constraints.csv',
+    'player': 'levels/1/level_1_player.csv',
+    'grass': 'levels/1/level_1_grass.csv',
     'node_pos': (400,610),
 	'unlock': 3,
 	'node_graphics': './graphics/overworld/3',
